805e94681c9bd0e33335a648e73460615a0de2ca
Daryl Fung <darylfung96@gmail.com>
2022-08-12 22:15:29 -0500

    added count

--- click.py
+++ click.py
@@ -1,12 +1,18 @@
 
 from sites import NeoBux
+from argparse import ArgumentParser
+
+
+parser = ArgumentParser()
+parser.add_argument('--starting', default=0)
+args = parser.parse_args()
 
 
 if __name__ == '__main__':
 	with open('data/gmail acc.bak', 'r') as f:
 		content = f.read()
 	email_list = content.split()
-	email_list = [email.split(":") for email in email_list]
+	email_list = [email.split(":") for email in email_list][args.starting:]
 	for i, email in enumerate(email_list):
 		neobux = NeoBux()
 		username = email[0].split('@')[0]
